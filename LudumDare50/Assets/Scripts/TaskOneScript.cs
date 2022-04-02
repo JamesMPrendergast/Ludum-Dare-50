e48d0326b4038639900abdcffe6774749e2d9275
growlingriffin <89081468+growlingriffin@users.noreply.github.com>
2022-04-02 11:44:00 -0500

    Task System Revised
    Made it so one script can manage any task, added triggers, expiration time.

--- LudumDare50/Assets/Scripts/TaskOneScript.cs
+++ LudumDare50/Assets/Scripts/TaskOneScript.cs
@@ -6,18 +6,33 @@ using UnityEngine;
 //This script automatically initializes Task1
 public class TaskOneScript : MonoBehaviour
 {
-    Vector3[] positions = { new Vector3(2, 0, 2), new Vector3(0, 0, 2), new Vector3(-2, 0, 2) };
+    public Vector3[] positions = { new Vector3(2, 0, 2), new Vector3(0, 0, 2), new Vector3(-2, 0, 2) };
+    private float timer;
 
+    //time until task expires
+    public float expirationThreshold = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
         //assign random position from list of possible ones
-        transform.position = positions[Random.Range(0, 3)];
+        transform.position = positions[Random.Range(0, positions.Length)];
+        timer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= expirationThreshold)
+        {
+            //task expires
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //task complete
+        Destroy(gameObject);
     }
 }
