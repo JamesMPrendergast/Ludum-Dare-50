adfe3a540967abfe640c7d8be172dd918c05752b
JamesMPrendergast <james.prendergast@kinkaid.org>
2022-04-03 20:28:33 -0500

    Game Over Screen + Highscore

--- LudumDare50/Assets/Scripts/ButtonScript.cs
+++ LudumDare50/Assets/Scripts/ButtonScript.cs
@@ -5,16 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ButtonScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerPrefs.DeleteAll();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void StartGame()
--- LudumDare50/Assets/Scripts/GameOverText.cs
+++ LudumDare50/Assets/Scripts/GameOverText.cs
@@ -6,15 +6,38 @@ using UnityEngine.UI;
 public class GameOverText : MonoBehaviour
 {
     int minutes, seconds;
+    public Text highscoreText;
+    int highscoreMinutes, highscoreSeconds;
 
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
         minutes = PlayerPrefs.GetInt("minutes", 0);
         seconds = PlayerPrefs.GetInt("seconds", 0);
 
-        gameObject.GetComponent<Text>().text = "You survived for " + minutes + " minute(s) and " + seconds + " second(s)!";
- 
+        gameObject.GetComponent<Text>().text = minutes + ":" + seconds;
+
+        highscoreMinutes = PlayerPrefs.GetInt("highscoreMinutes", 0);
+        highscoreSeconds = PlayerPrefs.GetInt("highscoreSeconds", 0);
+
+        float score = minutes * 60 + seconds;
+        float highScore = highscoreMinutes * 60 + highscoreSeconds;
+
+        PlayerPrefs.DeleteAll();
+
+        if (score > highScore) {
+            highscoreText.text = "New highscore!";
+            PlayerPrefs.SetInt("highscoreMinutes", minutes);
+            PlayerPrefs.SetInt("highscoreSeconds", seconds);
+        }
+        else {
+            highscoreText.text = "Highscore: " + highscoreMinutes + ":" + highscoreSeconds;
+            PlayerPrefs.SetInt("highscoreMinutes", highscoreMinutes);
+            PlayerPrefs.SetInt("highscoreSeconds", highscoreSeconds);
+        }
     }
 
     // Update is called once per frame
